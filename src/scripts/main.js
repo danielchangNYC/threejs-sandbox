a1cf4416b9512e16fdfa182627fdefa82f6d66b1
Daniel Chang <daniel.chang85@gmail.com>
2017-03-15 12:32:09 -0400

    Render three js canvas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('js');
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+} from 'three';
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
