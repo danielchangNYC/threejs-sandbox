5804daf1d016032058f1cc69171514418a91e881
Daniel Chang <daniel.chang85@gmail.com>
2017-03-14 23:27:37 -0400

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,62 @@
+'use strict';
+
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
+import { create as browserSyncCreate } from 'browser-sync';
+import browserify from 'browserify';
+import uglify from 'gulp-uglify';
+import gutil from 'gulp-util';
+
+const browserSync = browserSyncCreate();
+
+gulp.task('sass', () => {
+  gulp
+    .src('src/styles/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('app.css'))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('build', () => {
+  browserify({
+    entries: 'src/scripts/main.js',
+    debug: true,
+    transform: ['babelify'],
+  })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/js/'));
+});
+
+gulp.task('js-watch', ['build'], done => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('serve', ['sass', 'js-watch'], () => {
+  browserSync.init({
+    server: {
+      baseDir: './public',
+    },
+  });
+
+  gulp.watch('src/**/*.js', ['js-watch']);
+  gulp.watch('src/styles/*.scss', ['sass']);
+  gulp.watch('public/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
